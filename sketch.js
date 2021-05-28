7d3a9794fa4448957c07da82180def82829b46ca
heycupcak3 <84944996+heycupcak3@users.noreply.github.com>
2021-05-28 05:12:41 +0200

    added files from p5.js working space

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let img;
+let ima=new Image("data/Base/0A.webp");
+
+function preload(){
+ //img =loadImage('data/Base/0A.png'); 
+  
+}
+
+function setup() {
+let canvas= createCanvas(400, 400);
+  
+
+}
+
+function draw() {
+//canvas;
+  background(220);
+  //image(img, 0,0);
+  image(ima,0,0);
+}
